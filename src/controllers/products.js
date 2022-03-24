00e37781262c7647df687febc5ec9b131273a27f
Abdullokh <abdullohcoder@yandex.ru>
2022-03-25 01:56:20 +0500

    add sell & add routes

--- src/controllers/products.js
+++ src/controllers/products.js
@@ -28,6 +28,55 @@ class Products {
     }
   }
 
+  async SELL(req, res) {
+    try {
+      let { id, count, price } = req.body;
+      let total = 0;
+      const data = await BatchesSchema.find({ product_id: id });
+
+      for (let i = 0; i < data.length; i++) {
+        total += data[i].count;
+      }
+
+      if (total < count) {
+        res.status(400).json({
+          message: "product count is not enough",
+        });
+      }
+
+      for (let i = 0; count > 0; i++) {
+        if (count < data[i].count) {
+          async function a() {
+            let b = await BatchesSchema.findById({ product_id: id });
+
+            console.log(b);
+            b.count = 1;
+
+            b.save();
+          }
+
+          console.log(a());
+          BatchesSchema.findOneAndUpdate(
+            { _id: "623cb196deec6c9173f1e171" },
+            { $set: { price } }
+          );
+
+          count = 0;
+        }
+      }
+
+      res.status(200).json({
+        message: "ok",
+        data: "ok",
+      });
+    } catch (error) {
+      console.log(error);
+      res.status(400).json({
+        message: error.message,
+      });
+    }
+  }
+
   async GET(_, res) {
     try {
       const products = await ProductSchema.find({});
--- src/routes/products.js
+++ src/routes/products.js
@@ -4,5 +4,6 @@ const products = require("../controllers/products");
 
 router.get("/left", products.GET);
 router.post("/add", products.POST);
+router.post("/sell", products.SELL);
 
 module.exports = router;
